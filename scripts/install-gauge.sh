17805cdb10a6f4e61625f19f365c8f5222eb008d
John Boyes <john@agilepathway.co.uk>
2020-03-31 22:34:14 +0700

    Deploy image to Docker Hub
    Based on:
    https://circleci.com/blog/using-circleci-workflows-to-replicate-docker-hub-automated-builds/

--- scripts/install-gauge.sh
+++ scripts/install-gauge.sh
@@ -7,7 +7,12 @@ gauge install xml-report
 
 # test/verify installation
 if gauge --version; then
-    echo "Gauge $(gauge --version) has been installed to $(which gauge)"
+    GAUGE_VERSION=$(echo "$GAUGE" | grep -Po '(?<=Gauge version: )\d.\d.\d' )
+    if $GAUGE_VERSION != "1.0.8"; then
+        echo "New version of Gauge: $GAUGE_VERSION - you must update the Docker tag to get the build to pass."
+        exit 1
+    fi
+    echo "$GAUGE_VERSION has been installed to $(which gauge)"
 else
     echo "Something went wrong; Gauge could not be installed"
     exit 1
---
+++ scripts/tag-and-conditionally-publish-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+CIRCLECI_TAG="CIRCLECI-${CIRCLE_BUILD_NUM}"
+GAUGE_VERSION="1.0.8"
+GAUGE_TAG="GAUGE-$GAUGE_VERSION"
+GIT_TAG="GIT-COMMIT-${CIRCLE_SHA1}"
+NODE_VERSION=13.11
+NODE_TAG="NODE-${NODE_VERSION}"
+echo "Using the Gauge version as the semantic version of the image, as that is what CircleCI do
+      for their base images: https://circleci.com/docs/2.0/circleci-images/#best-practices"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_VERSION"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$CIRCLECI_TAG"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_TAG"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GIT_TAG"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$NODE_TAG"
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+      docker push "$IMAGE_NAME"
+fi
