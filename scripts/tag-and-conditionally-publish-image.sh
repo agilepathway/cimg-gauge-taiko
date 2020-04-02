34b2a530e4725707d7537eda13b622328ceec872
John Boyes <john@agilepathway.co.uk>
2020-04-02 08:34:59 +0700

    Update Node.js to v13.12

--- scripts/tag-and-conditionally-publish-image.sh
+++ scripts/tag-and-conditionally-publish-image.sh
@@ -5,7 +5,7 @@ CIRCLECI_TAG="CIRCLECI-${CIRCLE_BUILD_NUM}"
 GAUGE_VERSION="1.0.8"
 GAUGE_TAG="GAUGE-$GAUGE_VERSION"
 GIT_TAG="GIT-COMMIT-${CIRCLE_SHA1}"
-NODE_VERSION=13.11
+NODE_VERSION=13.12
 NODE_TAG="NODE-${NODE_VERSION}"
 echo "Using the Gauge version as the semantic version of the image, as that is what CircleCI do
       for their base images: https://circleci.com/docs/2.0/circleci-images/#best-practices"
