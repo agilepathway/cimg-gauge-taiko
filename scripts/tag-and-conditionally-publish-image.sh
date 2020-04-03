328f2f72020a4dbf018f512e02f2b77fb92336b5
John Boyes <john@agilepathway.co.uk>
2020-04-03 15:14:26 +0700

    Add ShellCheck validation of bash scripts (#19)
    https://circleci.com/docs/2.0/using-shell-scripts/#use-shellcheck
    https://shellcheck.net

--- scripts/tag-and-conditionally-publish-image.sh
+++ scripts/tag-and-conditionally-publish-image.sh
@@ -15,7 +15,7 @@ NODE_TAG="NODE-${NODE_VERSION}"
 docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_VERSION"
 # Also tag the Gauge version and the circle build together so that consumers can pin to an 
 # idempotent image
-docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_VERSION"-"$CIRCLECI_TAG"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_VERSION-$CIRCLECI_TAG"
 docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$CHROME_TAG"
 docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$CIRCLECI_TAG"
 docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$GAUGE_TAG"
