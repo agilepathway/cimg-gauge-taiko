ed45e25f371d76da74e11be9177fdd3d135ea160
John Boyes <john@agilepathway.co.uk>
2020-04-07 10:15:28 +0700

    Dynamically tag Docker image with Gauge version
    To get this working it was important to remove the unnecessary `-t`
    tty flag from the `docker run` command.  When running with tty
    then a carriage return is added at the end of every line. [1][2]
    During the build pipeline we do not need an interactive terminal so we
    should not be using the tty option anyway.
    
    [1] https://stackoverflow.com/a/55109365
    [2] https://github.com/moby/moby/issues/8513#issuecomment-216269864

--- scripts/tag-and-conditionally-publish-image.sh
+++ scripts/tag-and-conditionally-publish-image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-FULL_CHROME_VERSION=$(docker run --rm -it "$IMAGE_NAME" bash -c 'echo "$(google-chrome --version)"')
+FULL_CHROME_VERSION=$(docker run --rm -i "$IMAGE_NAME" bash -c 'echo "$(google-chrome --version)"')
 CHROME_VERSION=${FULL_CHROME_VERSION//[!0-9.]/}
 CHROME_TAG="CHROME-${CHROME_VERSION}"
 CIRCLECI_TAG="CIRCLECI-${CIRCLE_BUILD_NUM}"
-GAUGE_VERSION="1.0.8"
+GAUGE_VERSION=$(docker run --rm -i "$IMAGE_NAME" bash -c 'echo "$(gauge-version)"')
 GAUGE_TAG="GAUGE-$GAUGE_VERSION"
 GIT_TAG="GIT-COMMIT-${CIRCLE_SHA1}"
 NODE_VERSION=13.12
