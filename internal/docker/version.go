47c0ca9d7eaa441ff10d1c87e108ca5cb1e2113b
John Boyes <john@agilepathway.co.uk>
2020-04-22 12:18:01 +0700

    Refactor go tagging and version lookup (#77)
    Created a new `image` type which gets instantiated with the image name.
    Closes #49
    
    Lookup the version numbers just once, when initialising the new image
    type.
    Closes #47

--- internal/docker/version.go
+++ internal/docker/version.go
@@ -11,17 +11,17 @@ type softwareVersion struct {
 	dummy     string
 }
 
-func (sv softwareVersion) findAndTrim() string {
-	verboseVersion := sv.findVerbose(sv.command)
+func (sv softwareVersion) findAndTrim(image string) string {
+	verboseVersion := sv.findVerbose(image, sv.command)
 	return sv.trim(verboseVersion)
 }
 
-func (sv softwareVersion) findVerbose(command string) string {
+func (sv softwareVersion) findVerbose(image, command string) string {
 	if doesEnvVarExist("INTEGRATION_TEST") {
 		return sv.dummy
 	}
 
-	return execute(command)
+	return execute(image, command)
 }
 
 func (sv softwareVersion) trim(verbose string) string {
@@ -34,7 +34,7 @@ func doesEnvVarExist(envVar string) bool {
 	return exists
 }
 
-func gaugeVersion() string {
+func gaugeVersion(image string) string {
 	return softwareVersion{
 		command:   "gauge --version",
 		trimRegex: `Gauge version: (\d+(.\d+)?(.\d+)?)`,
@@ -46,37 +46,37 @@ Plugins
 html-report (4.0.6)
 js (2.3.4)
 screenshot (0.0.1)`}.
-		findAndTrim()
+		findAndTrim(image)
 }
 
-func chromeVersion() string {
+func chromeVersion(image string) string {
 	return softwareVersion{
 		command:   "google-chrome --version",
 		trimRegex: `Google Chrome ([0|1-9|\.]*)`,
 		dummy:     "Google Chrome 81.0.4044.92"}.
-		findAndTrim()
+		findAndTrim(image)
 }
 
-func goVersion() string {
+func goVersion(image string) string {
 	return softwareVersion{
 		command:   "go version",
 		trimRegex: `go version go(\d+(.\d+)?(.\d+)?)`,
 		dummy:     "go version go1.0.2 darwin/amd64"}.
-		findAndTrim()
+		findAndTrim(image)
 }
 
-func nodeVersion() string {
+func nodeVersion(image string) string {
 	return softwareVersion{
 		command:   "node --version",
 		trimRegex: `v(\d+(.\d+)?(.\d+)?)`,
 		dummy:     "v1.0.3"}.
-		findAndTrim()
+		findAndTrim(image)
 }
 
-func taikoVersion() string {
+func taikoVersion(image string) string {
 	return softwareVersion{
 		command:   "npm ls taiko -global -parseable -long",
 		trimRegex: `taiko@(\d+(.\d+)?(.\d+)?)`,
 		dummy:     "/home/circleci/.npm-global/lib/node_modules/taiko:taiko@1.0.4:undefined"}.
-		findAndTrim()
+		findAndTrim(image)
 }
--- magefile.go
+++ magefile.go
@@ -6,15 +6,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/agilepathway/cimg-gauge/internal/docker"
 )
 
 var Default = DockerTags
 
-// Returns the Docker tags which should be added to the Docker image
+// DockerTags returns the list of Docker tags which the image should then be tagged with.
+// The list includes version numbers of the key software installed on the image.
 func DockerTags() {
 	log.Println("Getting the Docker tags ...")
-
-	fmt.Print(docker.Tags())
+	imageName := os.Getenv("IMAGE_NAME")
+	fmt.Print(docker.TagsForImage(imageName))
 }
